b9c667fd464cb7341ed08b7ee994ca1ecb48ce65
Skeyling1 <skeyling1@gmail.com>
2024-12-29 13:38:03 +0300

    ccc

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
-import unittest
+import pytest
+from main import check
 
 
-class TestMath(unittest.TestCase):
-    def test_add(self):
-        self.
+def test_check():
+    assert check(6) == True
+
+
+def test_check2():
+    assert check(3) == False
+
+
+@pytest.mark.parametrize("number, expected", [
+    (2, True),
+    (5, False),
+    (0, True),
+    (22, True),
+    (-8, True)
+])
+def test_check_with_param(number, expected):
+    assert check(number) == expected
+
+
+if __name__ == "__main__":
+    unittest.main()
